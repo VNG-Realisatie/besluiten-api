7a6d7c6fd5df3654df9a3eb9be4812bb66be3181
Sergei Maertens <sergei@maykinmedia.nl>
2022-04-20 19:36:54 +0200

    :green_heart: Fixing tests after vng-api-common upgrade

--- src/brc/datamodel/tests/factories.py
+++ src/brc/datamodel/tests/factories.py
@@ -21,7 +21,7 @@ class BesluitFactory(factory.django.DjangoModelFactory):
             end_date=self.datum + timedelta(days=180),
             tzinfo=timezone.utc,
         )
-        return _ingangsdatum.evaluate(self, None, None).date()
+        return _ingangsdatum.evaluate(self, None, {"locale": _ingangsdatum._defaults["locale"]}).date()
 
     class Params:
         with_etag = factory.Trait(
