76e341efccf0d10b61cc2f3fc7b72bbc56486025
matthijsbekendam <matthijsbekendam@gmail.com>
2022-08-11 10:39:46 +0200

    setup npm and update requirements

--- bin/compile_dependencies.sh
+++ bin/compile_dependencies.sh
@@ -6,15 +6,21 @@ toplevel=$(git rev-parse --show-toplevel)
 
 cd $toplevel
 
+export CUSTOM_COMPILE_COMMAND="./bin/compile_dependencies.sh"
+
 # Base deps
 pip-compile \
-    --no-index \
+    --no-emit-index-url \
+    --allow-unsafe \
+    "$@" \
     requirements/base.in
 
 # Jenkins/tests deps
 pip-compile \
-    --no-index \
+    --no-emit-index-url \
+    --allow-unsafe \
     --output-file requirements/jenkins.txt \
+    "$@" \
     requirements/base.txt \
     requirements/testing.in \
     requirements/jenkins.in
