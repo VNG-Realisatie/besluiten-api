c73bebb7db260510fbdda0bd666b7ae414ef4f7a
matthijsbekendam <matthijsbekendam@gmail.com>
2022-08-16 17:10:05 +0200

    Changes after code review Sonny

--- bin/compile_dependencies.sh
+++ bin/compile_dependencies.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
+##!/bin/bash
+#
+# Compile the dependencies for production, CI and development.
+#
+# Usage, in the root of the project:
+#
+#     ./bin/compile_dependencies.sh
+#
+# Any extra flags/arguments passed to this wrapper script are passed down to pip-compile.
+# E.g. to update a package:
+#
+#     ./bin/compile_dependencies.sh --upgrade-package django
 
 set -ex
 
@@ -15,7 +26,26 @@ pip-compile \
     "$@" \
     requirements/base.in
 
-# Jenkins/tests deps
+# Production deps
+pip-compile \
+    --no-emit-index-url \
+    --allow-unsafe \
+    --output-file requirements/production.txt \
+    "$@" \
+    requirements/base.txt \
+    requirements/production.in
+
+# Dev deps
+pip-compile \
+    --no-emit-index-url \
+    --allow-unsafe \
+    --output-file requirements/dev.txt \
+    "$@" \
+    requirements/base.txt \
+    requirements/testing.in \
+    requirements/dev.in
+
+# Jenkins deps
 pip-compile \
     --no-emit-index-url \
     --allow-unsafe \
--- src/brc/tests/test_notifications_kanaal.py
+++ src/brc/tests/test_notifications_kanaal.py
@@ -26,7 +26,7 @@ class CreateNotifKanaalTestCase(APITestCase):
         call_command(
             "register_kanaal",
             "kanaal_test",
-            notificaties_api_root="https://example.com/api/v1",
+            nc_api_root="https://example.com/api/v1",
             stdout=stdout,
         )
 
@@ -53,7 +53,7 @@ class CreateNotifKanaalTestCase(APITestCase):
         stdout = StringIO()
         call_command(
             "register_kanaal",
-            notificaties_api_root="https://example.com/api/v1",
+            nc_api_root="https://example.com/api/v1",
             stdout=stdout,
         )
 
