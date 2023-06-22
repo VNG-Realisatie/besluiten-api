861c4b72b785ad6f94f4a000dd2764f69bbc18b1
matthijsbekendam <matthijsbekendam@gmail.com>
2023-06-22 13:52:22 +0200

    fix redis url

--- src/brc/conf/docker.py
+++ src/brc/conf/docker.py
@@ -41,7 +41,7 @@ CACHES = {
     # Cache for BIO removal sync with DRC
     "drc_sync": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": f"redis://{getenv('REDIS_CACHE')}",  # NOTE: watch out for multiple projects using the same cache!
+        "LOCATION": f"redis://{os.getenv('REDIS_CACHE','127.0.0.1:6379')}",  # NOTE: watch out for multiple projects using the same cache!
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "IGNORE_EXCEPTIONS": True,
